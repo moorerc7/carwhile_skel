c8bb2df3685c9cce545baab9d99a4a27fad51d1e
Ryan Moore <ryan@carwhile.com>
2017-04-03 19:43:22 -0400

    added JS file and sticky nav feature

---
+++ assets/javascript/application.js
@@ -0,0 +1,18 @@
+// strict mode
+'use strict';
+
+// Sticky Nav Feature
+$(document).ready(function () {
+	if ($(window).scrollTop() !== 0) {
+		$('.header_v2').addClass('sticky');
+	}
+
+	$(window).on('scroll', function() {
+		$('.header_v2').addClass('sticky');
+		if ($(this).scrollTop() == 0) {
+			$('.header_v2').removeClass('sticky');
+		}
+	});
+});
+
+
