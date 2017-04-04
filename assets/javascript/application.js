42ceaaf0e6e0351a9d980daea49451ab52777617
Ryan Moore <ryan@carwhile.com>
2017-04-04 11:31:08 -0400

    try 2 sticky nav

--- assets/javascript/application.js
+++ assets/javascript/application.js
@@ -2,17 +2,33 @@
 'use strict';
 
 // Sticky Nav Feature
-$(document).ready(function () {
-	if ($(window).scrollTop() !== 0) {
-		$('.header_v2').addClass('sticky');
+// $(document).ready(function () {
+// 	if ($(window).scrollTop() !== 0) {
+// 		$('.header_v2').addClass('sticky');
+// 	}
+
+// 	$(window).on('scroll' || 'touchmove', function() {
+// 		$('.header_v2').addClass('sticky');
+// 		if ($(this).scrollTop() == 0) {
+// 			$('.header_v2').removeClass('sticky');
+// 		}
+// 	});
+// });
+
+// Sticky nav try 2
+document.addEventListener("touchmove", ScrollStart, false);
+document.addEventListener("scroll", Scroll, false);
+
+function ScrollStart() {
+    $('.header_v2').addClass('sticky');
+    console.log('scrollStart working');
+}
+
+function Scroll() {
+	$('.header_v2').addClass('sticky');
+    console.log('scroll working for start');	
+	if ($(this).scrollTop() == 0) {
+		$('.header_v2').removeClass('sticky');
+		console.log('scroll end working');
 	}
-
-	$(window).on('scroll' || 'touchmove', function() {
-		$('.header_v2').addClass('sticky');
-		if ($(this).scrollTop() == 0) {
-			$('.header_v2').removeClass('sticky');
-		}
-	});
-});
-
-
+}
