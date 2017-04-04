db90433bbad037b833b774129c55ebfb0bbfa281
Ryan Moore <ryan@carwhile.com>
2017-04-04 13:39:06 -0400

    testing sticky again

--- assets/javascript/application.js
+++ assets/javascript/application.js
@@ -15,11 +15,20 @@ $(document).ready(function () {
 	});
 });
 
-// window size test
-$(document).ready(function() {
-	if (window.matchMedia("(min-width: 400px)").matches) {
-		console.log('I should add sticky now');
-	} else {
-		console.log('dont add sticky');
-	} 
-})
+// // Sticky nav feature
+// $(document).ready(function() {
+// 	if (window.matchMedia("(min-width: 550px)").matches) {
+// 		console.log('I should add sticky now');
+// 		if ($(window).scrollTop() !== 0) {
+// 			$('.header_v2').addClass('sticky');
+// 		}
+// 		$(window).on('scroll' || 'touchmove', function() {
+// 			$('.header_v2').addClass('sticky');
+// 			if ($(this).scrollTop() == 0) {
+// 				$('.header_v2').removeClass('sticky');
+// 			}
+// 		});
+// 	} else {
+// 		console.log('dont add sticky');
+// 	} 
+// })
