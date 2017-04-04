5cbc0d8ce5745918fa6ad3352501890efcf23a1c
Ryan Moore <ryan@carwhile.com>
2017-04-04 14:37:18 -0400

    hamburger fixed on sticky, only on larger than phablet

--- assets/javascript/application.js
+++ assets/javascript/application.js
@@ -1,34 +1,34 @@
 // strict mode
 'use strict';
 
-// Sticky Nav Feature
-$(document).ready(function () {
-	if ($(window).scrollTop() !== 0) {
-		$('.header_v2').addClass('sticky');
-	}
+// // Sticky Nav Feature first try
+// $(document).ready(function () {
+// 	if ($(window).scrollTop() !== 0) {
+// 		$('.header_v2').addClass('sticky');
+// 	}
 
-	$(window).on('scroll' || 'touchmove', function() {
-		$('.header_v2').addClass('sticky');
-		if ($(this).scrollTop() == 0) {
-			$('.header_v2').removeClass('sticky');
-		}
-	});
-});
-
-// // Sticky nav feature
-// $(document).ready(function() {
-// 	if (window.matchMedia("(min-width: 550px)").matches) {
-// 		console.log('I should add sticky now');
-// 		if ($(window).scrollTop() !== 0) {
-// 			$('.header_v2').addClass('sticky');
+// 	$(window).on('scroll' || 'touchmove', function() {
+// 		$('.header_v2').addClass('sticky');
+// 		if ($(this).scrollTop() == 0) {
+// 			$('.header_v2').removeClass('sticky');
 // 		}
-// 		$(window).on('scroll' || 'touchmove', function() {
-// 			$('.header_v2').addClass('sticky');
-// 			if ($(this).scrollTop() == 0) {
-// 				$('.header_v2').removeClass('sticky');
-// 			}
-// 		});
-// 	} else {
-// 		console.log('dont add sticky');
-// 	} 
-// })
+// 	});
+// });
+
+// Sticky nav feature
+$(document).ready(function() {
+	if (window.matchMedia("(min-width: 550px)").matches) {
+		console.log('I should add sticky now');
+		if ($(window).scrollTop() !== 0) {
+			$('.header_v2').addClass('sticky');
+		}
+		$(window).on('scroll' || 'touchmove', function() {
+			$('.header_v2').addClass('sticky');
+			if ($(this).scrollTop() == 0) {
+				$('.header_v2').removeClass('sticky');
+			}
+		});
+	} else {
+		console.log('dont add sticky');
+	} 
+});
