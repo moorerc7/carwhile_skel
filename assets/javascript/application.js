f03de285b0714195d4edd72890038d1a444d36d1
Ryan Moore <ryan@carwhile.com>
2017-04-10 13:08:03 -0400

    login page almost done, need to fix header

--- assets/javascript/application.js
+++ assets/javascript/application.js
@@ -31,16 +31,46 @@ $(document).ready(function() {
 		$(this).closest('.header_v2').find('.nav_panel_mobile').toggleClass('expand');
 		$(this).closest('body').toggleClass('expand');
 	});
-})
+});
 
 /*--------------------------------------LOGIN PAGE---------------------------------------*/
 
 // SWITCH BETWEEN LOGIN AND SIGN UP FORMS
 $(document).ready(function() {
-	$('.login_tabs_container').on('click', 'label', function() {
-		console.log('LOGIN / SIGNUP BUTTON WORKS');
-		// ABOVE LINE IS FOR TESTING BE SURE TO REMOVE ONCE DONE
-		$(this).parent('li').toggleClass('is_active');
-		$(this).parent('li').siblings().toggleClass('is_active');
-	})
-})
+	$('.login_tabs_container').on('click', '.login_label', function() {
+		var loginTab = $(this).parent('li');
+		var loginForm = $('.log_in_form');
+		var siblingTab = $(this).parent('li').siblings();
+		var signupForm = $('.sign_up_form');
+		if (loginTab.hasClass('is_active') && !loginForm.hasClass('hidden_form')) {
+			console.log('LOGIN POSITIVE IF STATEMENT WORKS!');
+			// ABOVE LINE IS FOR TESTING BE SURE TO REMOVE ONCE DONE
+		} else {
+			console.log('LOGIN ELSE STATEMENT WORKS!');
+			// ABOVE LINE IS FOR TESTING BE SURE TO REMOVE ONCE DONE
+			loginTab.toggleClass('is_active');
+			loginForm.toggleClass('hidden_form');
+			siblingTab.toggleClass('is_active');
+			signupForm.toggleClass('hidden_form');
+		}
+	});
+
+	$('.login_tabs_container').on('click', '.signup_label', function() {
+		var signupTab = $(this).parent('li');
+		var signupForm = $('.sign_up_form');
+		var siblingTab = $(this).parent('li').siblings();
+		var loginForm = $('.log_in_form');
+		if (signupTab.hasClass('is_active') && !signupForm.hasClass('hidden_form')) {
+			console.log('SIGNUP IF STATEMENT WORKS!');
+			// ABOVE LINE IS FOR TESTING BE SURE TO REMOVE ONCE DONE
+		} else {
+			console.log('SIGNUP ELSE STATEMENT WORKS!');
+			// ABOVE LINE IS FOR TESTING BE SURE TO REMOVE ONCE DONE
+			signupTab.toggleClass('is_active');
+			signupForm.toggleClass('hidden_form');
+			siblingTab.toggleClass('is_active');
+			loginForm.toggleClass('hidden_form');
+		}
+	});
+});
+
